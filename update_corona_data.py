b0fabe02ef3738d74f07beb66f28e3b6aa3db253
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-07-08 21:35:48 -0600

    Updated data 08/07/2020

---
+++ update_corona_data.py
@@ -0,0 +1,13 @@
+import subprocess
+import shutil, os
+from pathlib import Path
+
+p = Path.home()
+covid_charts_dir = p / 'Documents/Workspace/NodeJs/covid-charts' 
+covid_map_app_dir = p / 'Documents/Workspace/NodeJs/covid-map-app'
+
+
+
+#subprocess.Popen("cd ..", shell = True)
+#subprocess.Popen("git commit ", shell = True)
+#subprocess.Popen("dir ", shell = True)
