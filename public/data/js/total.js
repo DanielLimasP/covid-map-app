200f4a013bc251a5a9bfbbc7a9e161523d14a574
Daniel Limas <amedlimas97@gmail.com>
2020-11-05 21:29:29 -0700

    5/11/2020

--- public/data/js/total.js
+++ public/data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":47331327,"deaths":1208654,"recovered":31434282,"date":"2020-11-03"}
+var covid_total_current = {"confirmed":47404173,"deaths":1213693,"recovered":31608668,"date":"2020-11-04"}
--- public/data/js/us_total.js
+++ public/data/js/us_total.js
@@ -1,1 +1,1 @@
-var covid_us_total_current = {"confirmed":9224926,"deaths":230716,"recovered":3641614,"date":"2020-11-03"}
+var covid_us_total_current = {"confirmed":9530444,"deaths":233943,"recovered":3745856,"date":"2020-11-06"}
