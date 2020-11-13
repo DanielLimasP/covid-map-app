b2ff354a214131d44660b46d4d21eeb892e60377
Daniel Limas <amedlimas97@gmail.com>
2020-11-12 22:12:12 -0700

    Added ansi colors

--- update_corona_data.py
+++ update_corona_data.py
@@ -8,6 +8,12 @@ import time
 import send2trash
 from pathlib import Path
 
+# ANSI COLORS:
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+
 home_dir = Path.home()
 corona_charts_dir = home_dir / 'Documents/Workspace/nodejs/covid-charts'
 corona_map_app_dir = home_dir / 'Documents/Workspace/nodejs/covid-map-app'
@@ -25,13 +31,13 @@ def display_files():
     # Display the files inside data folder of covid-charts
     print()
     for folder, subfolders, files in os.walk(corona_map_app_dir / 'public' / 'data'):
-        print("Folder {}".format(folder))
+        print(GREEN + "mFolder {}".format(folder))
 
         for subfolder_file in subfolders:
-            print("Subfolder {}".format(subfolder_file))
+            print(LIGHT_GRAY + "Subfolder {}".format(subfolder_file))
 
         for file in files:
-            print("File {}".format(file))
+            print(LIGHT_GRAY + "File {}".format(file))
 
         print()
 
@@ -42,16 +48,16 @@ def copy_data():
             shutil.rmtree(public_data)
         shutil.copytree(corona_charts_dir / 'data', corona_map_app_dir / 'public' / 'data')
     else:
-        print("At least one of the needed folders is absent. Please relocate it or clone the repository again")
+        print(RED + "At least one of the needed folders is absent. Please relocate it or clone the repository again")
 
-    print("Copying process finished!")
+    print(CYAN + "Copying process finished!")
 
 def update_repo():
     hour, minute, dd, mm, yyyy = get_date()
     if len(str(minute)) == 1:
         minute = "0" + str(minute)
     d = "{}:{} {} {} {}".format(hour, minute, dd, mm, yyyy)
-    #print("The date is: {}:{} {} {} {} ".format(hour, minute, dd, mm, yyyy))
+    print(CYAN + "The current time and date is: {}:{} {} {} {} ".format(hour, minute, dd, mm, yyyy))
     # Commit and display the status of the repo
     subprocess.Popen("git add *", shell=True)
     time.sleep(2)
@@ -61,7 +67,7 @@ def update_repo():
     time.sleep(2)
     subprocess.Popen("git push origin master", shell=True)
     time.sleep(14)
-    print("Done!")
+    print(GREEN + "Done!")
 
 if __name__ == "__main__":
     copy_data()
