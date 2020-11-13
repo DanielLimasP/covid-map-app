eb0d9dba792fcaab803f303556b0198e0202f92f
Daniel Limas <amedlimas97@gmail.com>
2020-11-12 19:01:25 -0700

    19:01 12/11/2020

--- update_corona_data.py
+++ update_corona_data.py
@@ -54,7 +54,7 @@ def update_repo():
     time.sleep(2)
     subprocess.Popen("git status", shell = True)
     time.sleep(2)
-    subprocess.Popen("git commit -a -m \"{}:{}/{}/{}/{}\"".format(hour, minute, dd, mm, yyyy), shell = True)
+    subprocess.Popen("git commit -a -m \"{}:{} {}/{}/{}\"".format(hour, minute, dd, mm, yyyy), shell = True)
     time.sleep(2)
     subprocess.Popen("git push origin master", shell = True)
     time.sleep(8)
