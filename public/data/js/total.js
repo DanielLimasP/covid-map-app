610482759fb90924959400e2e027b78e1539452d
Daniel Limas <amedlimas97@gmail.com>
2020-11-17 15:05:26 -0700

    15:05 17/11/2020

--- public/data/js/total.js
+++ public/data/js/total.js
@@ -1,1 +1,1 @@
-var covid_total_current = {"confirmed":54563236,"deaths":1319882,"recovered":35058846,"date":"2020-11-16"}
+var covid_total_current = {"confirmed":55400024,"deaths":1333632,"recovered":35617424,"date":"2020-11-17"}
